7726a6ff30db3301ff666a72468a042b5d5cafff
mohini <mohineeborate@gmail.com>
2026-02-27 19:23:35 +0530

    garden

---
+++ script.js
@@ -0,0 +1,7 @@
+function learnMore() {
+    alert("Welcome to Green Paradise Garden! 🌿 We offer the best plants and landscaping services.");
+}
+
+function showOffer() {
+    alert("🌸 Special Offer: Get 20% off on all plants this week!");
+}
